4f42c153f3afc918be22e465b641882aa6276b8b
cmgraff <cm0graff@gmail.com>
2018-10-10 22:33:55 -0500

    make a more robust test for atexit

--- tests-emperical/atexit.c
+++ tests-emperical/atexit.c
@@ -4,11 +4,27 @@
 void f(void)
 {
 	fprintf(stderr, "The atexit handlers fired off\n");
+	fprintf(stderr, "Exit handle 1\n");
 }
+void g(void)
+{
+        fprintf(stderr, "The atexit handlers fired off\n");
+	fprintf(stderr, "Exit handle 2\n");
+}
+void h(void)
+{
+        fprintf(stderr, "The atexit handlers fired off\n");
+	fprintf(stderr, "Exit handle 3\n");
+}
+
 
 int main(void)
 {
 	atexit(f);
+	atexit(g);
+	atexit(h);
 	fprintf(stderr, "The program is above the atexit handler in main\n");
+	fprintf(stderr, "There should be a 3 2 1 countdown\n");
+	
 	return 0;
 }
