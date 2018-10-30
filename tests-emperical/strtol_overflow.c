50a4ff679dc65e4937b5c1544837902ed3fe70e3
graff <cm0graff@gmail.com>
2018-10-30 16:00:45 -0500

    large tests

--- tests-emperical/strtol_overflow.c
+++ tests-emperical/strtol_overflow.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 int main(void)
 {
 	long i = LONG_MAX;
@@ -18,6 +19,22 @@ int main(void)
 		fprintf(stderr, "strtol correctly set %s to LONG_MAX\n", s);
 	else
 		fprintf(stderr, "strtol did not set %s to LONG_MAX\n", s);
+	
+	long long i4 = LONG_MAX;
+	//i4++;
+	i4--;
+	long act4 = 0;
+	char s4[1000] = { 0 };
+	sprintf(s4, "%lld", i4);
+	act4 = strtol(s4, NULL, 10);
+	if (errno == ERANGE)
+		fprintf(stderr, "strtol correctly set ERANGE for %s\n", s4);
+	else
+		fprintf(stderr, "strtol did not set ERANGE for %s\n", s4);
+	if (act == LONG_MAX)
+		fprintf(stderr, "strtol correctly set %s to LONG_MAX\n", s4);
+	else
+		fprintf(stderr, "strtol did not set %s to LONG_MAX\n", s4);
 
 
 	long i1 = LONG_MIN;
