d43cb7d446c44e83b8ceaf81042050a71aa29282
graff <cm0graff@gmail.com>
2018-10-29 23:48:11 -0500

    another printf overflow test

---
+++ tests-emperical/printf_overflow2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void)
+{
+	size_t i = INT_MAX;
+	//i++;
+	char *s = malloc(i);
+	if (!(s))
+	{
+		fprintf(stderr, "unable to allocate enough memory\n");
+		return 0;
+	}
+	memset(s, 'A', i -1);
+	s[i] = 0;
+	size_t len = 0;
+	len = printf("%s", s, 1);
+	if (errno == EOVERFLOW)
+		fprintf(stderr, "printf overflow was caught\n");
+	else
+		fprintf(stderr, "printf overflow was not caught\n");
+	fprintf(stderr, "%zu ret len\n", len);
+	return 0;
+}
