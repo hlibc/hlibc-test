d54e339f14149683f251a5f26af8e3b8fc368e9c
graff <cm0graff@gmail.com>
2018-11-05 09:57:29 -0600

    fix broken test

--- tests-comparative/fscanf_string_1s.c
+++ tests-comparative/fscanf_string_1s.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
 	FILE *o = fopen("Makefile", "r");
+	char s[1024] = { 0 };
 	if (o == NULL) {
 		fprintf(stderr, "'Makefile' not found for %s\n", argv[0]);
 		return 1;
 	}
-	for(;fscanf(o, "%s", s) != EOF) {
+	for(;fscanf(o, "%s", s) != EOF;) {
 		printf("%s\n", s);
 	}
 	return 0;
