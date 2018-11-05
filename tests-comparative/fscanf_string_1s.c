05f4ddd5e40ddceb88269a62d8b07d385ff36fa8
graff <cm0graff@gmail.com>
2018-11-05 09:38:39 -0600

    rename ambiguous test suite files

--- tests-comparative/fscanf_string_1s.c
+++ tests-comparative/fscanf_string_1s.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	FILE *o = fopen("Makefile", "r");
 	if (o == NULL) {
-		fprintf(stderr, "'Makefile' not found for scanf_string\n");
+		fprintf(stderr, "'Makefile' not found for %s\n", argv[0]);
 		return 1;
 	}
 	for(;fscanf(o, "%s", s) != EOF) {
