dee40f84d6cadce7911c7514fbcf1dc6527a26d4
graff <cm0graff@gmail.com>
2018-11-06 21:29:41 -0600

    add an fscanf test for integers

--- tests-comparative/fscanf_string_and_int.c
+++ tests-comparative/fscanf_string_and_int.c
@@ -4,13 +4,13 @@ int main(int argc, char **argv)
 {
 	FILE *o = fopen("Makefile", "r");
 	char s[1024] = { 0 };
-	char t[1024] = { 0 };
+	int t = 0;
 	if (o == NULL) {
 		fprintf(stderr, "'Makefile' not found for %s\n", argv[0]);
 		return 1;
 	}
-	for(;fscanf(o, "%s%s", s, t) != EOF;) {
-		printf("%s%s\n", s, t);
+	for(;fscanf(o, "%s%d", s, &t) != EOF;) {
+		printf("%s%d\n", s, t);
 	}
 	return 0;
 }
